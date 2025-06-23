2a91d9ddfd43a0171a8debf96ac67dc2a713863e
Divyendu Prakash <divyenduprakash98@gmail.com>
2025-06-24 00:19:32 +0530

    We have added the division funtionality in the code

--- cal.sh
+++ cal.sh
@@ -1,4 +1,6 @@
-read -p "Enter first numeber"  x
-read -p "Enter second number"  y
+read -p "Enter first numeber: "  x
+read -p "Enter second number: "  y
 
-echo "The sum of numbers are: $(( x + y )) "
+echo "The sum of numbers are :  $(( x + y )) "
+echo "The Division of $x and $y is : $(( x/y ))"
+
